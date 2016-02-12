91b76a9a7e807de42c03482686ffff8e88036323
Yameng Zhou <yameng@Yamengs-MacBook-Pro-2.local>
2016-02-11 22:16:00 -0800

    push up the makefile template

---
+++ Compile_Test/hello_world.cpp
@@ -0,0 +1,10 @@
+
+#include "utils.h"
+
+
+int main(int argc, char** argv){
+
+	cout << "Welcome to the brave new world!!" << endl;
+	utils *u = new utils("this is a test practice for c++!");
+	return 0;
+}
---
+++ Compile_Test/utils.h
@@ -0,0 +1,20 @@
+#ifndef __UTILS_H__
+
+#define __UTILS_H__
+using namespace std;
+
+#include <string>
+#include <iostream>
+#include <stdlib>
+using namespace std;
+
+class utils{
+public:
+	utils(){}
+	utils(string input){
+		cout << "print out the input: " << input << endl;
+	}
+
+};
+
+#endif
